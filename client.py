6f4f9b2bcacc412a74a5812a92ec4f07dfed5c67
johan-wiguna <6181801018@student.unpar.ac.id>
2021-05-04 17:24:26 +0700

    TicTacToe GUI

--- client.py
+++ client.py
@@ -1,28 +1,59 @@
-import PySimpleGUI as sg
 import socket
+from tkinter import *
+from tkinter import messagebox
 
 LOCALHOST = socket.gethostbyname(socket.gethostname())
 PORT = 5050
 ADDR = (LOCALHOST, PORT)
 
-sg.theme('Dark Gray 5')
-layoutGame = [
-    [sg.Text("", justification='center', size=(50,3), key='_QUESTION_')],
-    [sg.Button("",'center',size=(25,5),key='_ANS1_')
-    , sg.Button("",'center', size=(25,5),key='_ANS2_')]
-]
-layoutResult = [
-    [sg.Text("RESULT", justification='center', size=(50,1))],
-    [sg.Text("Tim 1", justification='center', size=(25,1)),sg.Text("Tim 2", justification='center', size=(25,1))],
-    [sg.Text("", justification='center', size=(25,1), key='_RES_A_'), sg.Text("", justification='center', size=(25,1), key='_RES_B_')],
-    [sg.Text("", justification='center', size=(25,1), key='_WINNER_')]
-]
+clicked = True # True: Giliran X, False: Giliran O
+count = 0
 
-layout = [
-    [sg.Column(layoutGame, key='-COL1-'), 
-    sg.Column(layoutResult, visible=False, key='-COL2-')]
-]
-window = sg.Window('Kahoot!?', layout)
+def btn_clicked(b):
+    global clicked, count
+    if b["text"] == "":
+        if clicked == True:
+            b["text"] = "X"
+            clicked = False
+            count += 1
+        else:
+            b["text"] = "O"
+            b["fg"] = "red"
+            clicked = True
+            count += 1
+    else:
+        messagebox.showerror("Misclicked", "Please click an empty box.")
+
+root = Tk()
+root.title('Socket Tic Tac Toe')
+
+# Button untuk setiap index
+b0 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b0))
+b1 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b1))
+b2 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b2))
+
+b3 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b3))
+b4 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b4))
+b5 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b5))
+
+b6 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b6))
+b7 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b7))
+b8 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: btn_clicked(b8))
+
+# Memasukkan button ke dalam grid
+b0.grid(row=0, column=0)
+b1.grid(row=0, column=1)
+b2.grid(row=0, column=2)
+
+b3.grid(row=1, column=0)
+b4.grid(row=1, column=1)
+b5.grid(row=1, column=2)
+
+b6.grid(row=2, column=0)
+b7.grid(row=2, column=1)
+b8.grid(row=2, column=2)
+
+root.mainloop()
 
 while True:
     event, values = window.read()
